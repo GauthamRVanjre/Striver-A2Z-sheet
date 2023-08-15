1b60a7d62e6858c3740304fb2b72808162d25c81
Gautham Vanjre <vanjregautham@gmail.com>
2023-08-15 13:17:03 +0530

    step 1.5 problem 1.5.3 and 1.5.4

--- Stepper1/step5/printNto1.java
+++ Stepper1/step5/printNto1.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
-class print1toN{
+class printNto1{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        int i=1;
+        int i=n;
         printNos(i,n);
     }
 
     static void printNos(int i,int n){
-        if(i>n){
+        if(i<1){
             return;
         }
-        System.out.println(i);
-        printNos(i+1,n);
+        System.out.print(i+" ");
+        printNos(i-1,n);
     }
 }
